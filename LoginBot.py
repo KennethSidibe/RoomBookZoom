3664902a85f8de4f7295328dea97a7e6e67c86e0
Sidibe <Elstevehcks@gmail.com>
2022-09-20 17:37:31 -0400

    Implement simple login logic

--- LoginBot.py
+++ LoginBot.py
@@ -1,3 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
 class LoginBot():
 
-    # to complete
+    def login(self):
+
+        browser = webdriver.Chrome('/Users/kanekisidibe/Developer/Python/RoomBookZoom/driver/chromedriver')
+        browser.get(('https://accounts.google.com/ServiceLogin?'
+                     'service=mail&continue=https://mail.google'
+                     '.com/mail/#identifier'))
+        buttonXPath = '//*[@id="identifierNext"]/div/button'
+
+        usernameField = browser.find_element(By.ID, "identifierId")
+        usernameField.send_keys(self.username)
+        nextButton = browser.find_element(By.XPATH, buttonXPath)
+        nextButton.click()
+
+        input(":")
