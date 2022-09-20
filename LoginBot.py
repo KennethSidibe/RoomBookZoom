268d9fe22c5dbd1a0738a1018e0d1ee3bd39db19
Sidibe <Elstevehcks@gmail.com>
2022-09-20 18:40:05 -0400

    implement LoginBot for google signin, passed dectection test for chrome

--- LoginBot.py
+++ LoginBot.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from credentials import *
-import undetected_chromedriver as uc
+import undetected_chromedriver.v2 as uc
 from time import sleep
 
 
@@ -12,31 +12,31 @@ class LoginBot():
 
     def login(self):
 
-        # Options to maximisze windows
-        options = webdriver.ChromeOptions()
-        options.add_argument("start-maximized")
 
-        # Other options
-        options.add_experimental_option('useAutomationExtension', False)
-        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # Options
+        options = uc.ChromeOptions()
 
+        options.user_data_dir = "c:\\temp\\profile"
 
-        browser = webdriver.Chrome('/Users/kanekisidibe/Developer/Python/RoomBookZoom/driver/chromedriver',
-                                   chrome_options=options)
-        browser.get(('https://accounts.google.com/ServiceLogin?'
+        options.add_argument('--user-data-dir=c:\\temp\\profile2')
+
+        options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+
+        # creating the driver
+        driver = uc.Chrome(options=options)
+
+        # Opening the url
+        driver.get(('https://accounts.google.com/ServiceLogin?'
                      'service=mail&continue=https://mail.google'
                      '.com/mail/#identifier'))
 
-
         buttonXPath = '//*[@id="identifierNext"]/div/button'
 
-        usernameField = browser.find_element(By.ID, "identifierId")
-        time.sleep(3)
+        usernameField = driver.find_element(By.ID, "identifierId")
         usernameField.send_keys(username)
-        time.sleep(3)
 
-        nextButton = browser.find_element(By.XPATH, buttonXPath)
-        time.sleep(3)
+        nextButton = driver.find_element(By.XPATH, buttonXPath)
+
         nextButton.click()
 
         input(":")
