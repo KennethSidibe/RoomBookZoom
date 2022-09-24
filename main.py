1ad01096b2083c9d0fe3385da84cb013937c0a27
Sidibe <Elstevehcks@gmail.com>
2022-09-23 23:28:53 -0400

    Add img filepath and analyze method refactoring

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ from TextBot import *
 
 if __name__ == '__main__':
 
+    filepath = 'screencapture/testBooking2.png'
+
     bot = LoginBot()
 
     bookBot = BookBot()
 
     textBot = TextBot()
 
-    textBot.analyze()
+    textBot.analyze(filepath)
 
     # bookBot.login()
