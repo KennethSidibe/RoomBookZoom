506ad9195042c2ca91c204a90b5a61d10c23a105
Sidibe <Elstevehcks@gmail.com>
2022-09-26 22:36:04 -0400

    Add indicator for timeSlotAvailability

--- constant.py
+++ constant.py
@@ -1,3 +1,7 @@
 uoLoginUrl = 'https://login.microsoftonline.com/'
 uoBookRoomUrl = 'https://bibliorooms.uottawa.ca/'
 boundBoxPixelOffset = 40
+FIRST_HALF_INDICATOR = 'FIRST-HALF-FREE'
+SECOND_HALF_INDICATOR = 'SECOND-HALF-FREE'
+FULLY_RESERVABLE_INDICATOR = 'FREE'
+FULLY_BOOK_INDICATOR = 'FULL'
