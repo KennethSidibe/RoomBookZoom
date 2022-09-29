670ebe2bfdff1974d02d2685184e0a0f1156009e
Sidibe <Elstevehcks@gmail.com>
2022-09-29 03:34:19 -0400

    Add filepath variable for easy multitesting

--- main.py
+++ main.py
@@ -2,10 +2,22 @@
 from BookBot import *
 from LoginBot import *
 from TextBot import *
+import cv2
+import time
 
 if __name__ == '__main__':
 
-    filepath = 'screencapture/testBooking2.png'
+    mobileFilepath = 'screencapture/mobileBookingScreenshot.jpg'
+    smallCopyFilePath = 'screencapture/smallBookingSchedule.png'
+    filepath = 'screencapture/booking_hard_test.png'
+    filepath2 = 'screencapture/booking_full_2.png'
+    filepath3 = 'screencapture/booking_half_empty.png'
+    filepath4 = 'screencapture/booking_schedule_full.png'
+    filepath5 = 'screencapture/testBooking.png'
+    filepath6 = 'screencapture/testBooking2.png'
+    filepath7 = 'screencapture/booking_FTX525-MRT412.png'
+    filepath8 = 'screencapture/booking_MRT412-RGN1020.png'
+    testProcessFilePath = 'screencapture/testProcess.png'
 
     bot = LoginBot()
 
@@ -13,6 +25,12 @@ if __name__ == '__main__':
 
     textBot = TextBot()
 
-    textBot.analyze(filepath)
-
     # bookBot.login()
+
+    start = time.time()
+    availability = textBot.analyze(filepath8)
+    elapsedTime = time.time() - start
+
+    # print(availability)
+
+    print('elapsed time : ', elapsedTime)
